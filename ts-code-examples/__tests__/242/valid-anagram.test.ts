052ca71aa957dde5c079b29cc387260b8880c3a0
JackDiSalvatore <jackp.disalvatore@gmail.com>
2025-08-30 16:08:20 -0500

    remove .only
    Signed-off-by: JackDiSalvatore <jackp.disalvatore@gmail.com>

--- ts-code-examples/__tests__/242/valid-anagram.test.ts
+++ ts-code-examples/__tests__/242/valid-anagram.test.ts
@@ -1,6 +1,6 @@
 import { isAnagram } from "../../src/242/valid-anagram";
 
-describe.only("valid anagram", () => {
+describe("valid anagram", () => {
   it("should do something", () => {
     expect(isAnagram("racae", "aeacr")).toBe(true);
     expect(isAnagram("rat", "car")).toBe(false);
