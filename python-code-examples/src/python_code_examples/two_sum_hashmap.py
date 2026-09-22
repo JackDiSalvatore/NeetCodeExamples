dcf6b44b9ce0e09c4b57f37ddcc4305cf8856cd7
Jack DiSalvatore <C-Jack.DiSalvatore@charter.com>
2026-09-22 11:14:45 -0500

    deletes the inner if num == target - nums[complement_map[complement]] guard, which is tautological: complement_map[complement] is by construction the index of a value equal to complement, so target - complement == num always holds
    Signed-off-by: Jack DiSalvatore <C-Jack.DiSalvatore@charter.com>

--- python-code-examples/src/python_code_examples/two_sum_hashmap.py
+++ python-code-examples/src/python_code_examples/two_sum_hashmap.py
@@ -3,10 +3,17 @@
 
 # ex: nums = [1,1,2,3], target = 5
 
-# nums[0] (1) => complement is 4
-# nums[1] (1) => complement is 4
-# nums[2] (2) => complement is 3 (nums[3])
-# nums[3] (3) => complement is 2 (nums[2])
+# nums[0] = 1 => complement is 4
+# nums[1] = 1 => complement is 4
+# nums[2] = 2 => complement is 3 (nums[3])
+# nums[3] = 3 => complement is 2 (nums[2])
+
+# { num_complement_pair: index }
+# complement_map = {
+#   1: 0,
+#   1: 1,
+#   2: 2
+# }
 
 from pprint import pprint
 
@@ -15,39 +22,56 @@ DEBUG = False
 
 class TwoSumHashMapSolution:
 
-    def findIndex(self, nums, target):
+    def findIndex(self, nums: list[int], target: int) -> tuple[int, int]:
         if DEBUG:
             print(f"Target: {target}\r\nNums: {nums}\r\n")
 
         # "If you need x to add to the current number to make the target,
         # then look here in the nums arary"
         # { num_complement_pair: index }
-        complement_map = {}
+        complement_map: dict[int, int] = {}
 
         for idx, num in enumerate(nums):
             # print(f"idx: {idx}, num: {num}")
-            complement = target - num
-            complement_idx = complement_map.get(complement)
+            complement: int = target - num
 
-            if complement_idx == None:
+            complement_idx: int | None = complement_map.get(complement)
+
+            if DEBUG:
+                pprint(complement_map, width=1, indent=2, depth=2, sort_dicts=False)
+
+            if complement_idx is None:
                 complement_map[num] = idx
             else:
-                # 5 - nums[0] => 5 - 1 => 4
-                # 5 - nums[3] => 5 - 3 => 2 (search map for compl 2 => idx:2)
-                # print(target - nums[complement_map[complement]])
-                # print(f"nums[idx]: {complement_map[complement]}") # this should be idx[2] => 3
+                return complement_idx, idx
 
-                if num == target - nums[complement_map[complement]]:
-                    if DEBUG:
-                        print(
-                            f"Found {nums[complement_map[complement]]} and {nums[idx]}"
-                        )
-                        print(f"At: ({complement_map[complement]}, {idx})")
-                    return complement_map[complement], idx
+            #         return complement_map[complement], idx
 
-        if DEBUG:
-            print("--------------")
-            pprint(complement_map, width=1, indent=2, depth=2, sort_dicts=False)
-            print("--------------\r\n\r\n")
+        raise ValueError(f"no pair sums to {target}")
 
-        return 0, 0
+
+def main():
+    solution = TwoSumHashMapSolution()
+
+    # solution [complement of 1, complement of 3] => (1, 2)
+    # complement_map = {
+    #   1: 0,
+    #   1: 1,
+    #   2: 2
+    # }
+    x, y = solution.findIndex([1, 1, 2, 3], 5)
+    print(x, y)  # 1, 2
+
+    # solution [complement of 4, complement of 3] => (0, 1)
+    # complement_map = {
+    #   4: 0,
+    #   3: 1,
+    #   2: 2,
+    #   1: 3
+    # }
+    x, y = solution.findIndex([3, 4, 5, 6], 7)
+    print(x, y)  # 0, 1
+
+
+if __name__ == "__main__":
+    main()
